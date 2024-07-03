b82d3f926a48784a3d37d1b7e06cbb6a3e0be9bf
SRamanatha <140810477+SRamanatha@users.noreply.github.com>
2024-07-03 18:56:23 +0100

    Create master_worker_2.sh

---
+++ master_worker_2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+sudo apt install cri-tools
+
+sudo cat /etc/crictl.yaml <<EOF
+runtime-endpoint: unix:///run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+timeout: 2
+debug: true # <- if you don't want to see debug info you can set this to false
+pull-image-on-create: false
+EOF>>
+
+sudo systemctl enable kubelet
